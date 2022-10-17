1087b0bb0a204622e7e337d9f32e91b8540b1d26
Nikko Enggaliano <37658579+nikkoenggaliano@users.noreply.github.com>
2022-10-17 09:58:45 +0700

    Create gin_implementation.go

---
+++ Go/gin_implementation.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "everything is ok",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
